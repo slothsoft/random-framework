e8afd351c125fd480538038a359b5318206ec819
slothsoft <admin@slothsoft.de>
2020-01-09 23:52:37 +0100

    trying to fix the broken text files

---
+++ random/src/test/java/de/slothsoft/random/TextFileEncodingTest.java
@@ -0,0 +1,82 @@
+package de.slothsoft.random;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@Ignore
+@RunWith(Parameterized.class)
+public class TextFileEncodingTest {
+
+	@Parameters(name = "{1} ({0})")
+	public static Collection<Object[]> data() throws Exception {
+		final List<Object[]> result = new ArrayList<>();
+		addFiles(result, new File("src/main/resources"));
+		return result;
+	}
+
+	private static void addFiles(List<Object[]> result, File folder) throws Exception {
+		for (final File child : folder.listFiles()) {
+			if (child.isDirectory()) {
+				addFiles(result, child);
+			} else {
+				result.add(new Object[]{child, child.getParentFile().getName() + '/' + child.getName()});
+			}
+		}
+	}
+
+	private final File textFile;
+	private final String displayName;
+
+	public TextFileEncodingTest(File textFile, String displayName) {
+		this.textFile = textFile;
+		this.displayName = displayName;
+	}
+
+	@Test
+	public void testEncoding() throws Exception {
+		final List<String> lines = Files.readAllLines(this.textFile.toPath());
+
+		final List<String> brokenLines = new ArrayList<>();
+		for (final String line : lines) {
+			if (isBroken(line)) {
+				brokenLines.add(line);
+			}
+		}
+
+		if (!brokenLines.isEmpty()) {
+			System.out.println(this.displayName);
+			for (final String brokenLine : brokenLines) {
+				System.out.println("\t" + brokenLine);
+			}
+		}
+
+		Assert.assertEquals(Collections.emptyList(), brokenLines);
+	}
+
+	private static boolean isBroken(String input) {
+		for (final char c : input.toCharArray()) {
+			if (" '-//.`,&".contains(String.valueOf(c))) {
+				continue;
+			}
+			if (Character.isAlphabetic(c)) {
+				continue;
+			}
+			if (Character.isDigit(c)) {
+				continue;
+			}
+			return true;
+		}
+		return false;
+	}
+}
